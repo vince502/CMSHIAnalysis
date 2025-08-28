56f16a2cb0ce33d7834dc1be0337f5614dcb0316
soohwan <soohwan.lee@cern.ch>
2025-08-28 06:35:36 +0200

    test

--- HiSkim/HiOnia2EE/test/onia2EEPAT_PbPb_DATA_MiniAOD_cfg.py
+++ HiSkim/HiOnia2EE/test/onia2EEPAT_PbPb_DATA_MiniAOD_cfg.py
@@ -27,7 +27,7 @@ process.source = cms.Source("PoolSource",
 
 # Number of events
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(100)
 )
 
 # Output
